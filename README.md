29263851a3a901f9c678cd5f5f4c2a94e91a8c2d
jwlodek <jwlodek@cs.stonybrook.edu>
2019-11-01 13:41:08 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# py_cui-2048
+
+A CUI version of the 2048 game written with the help of the [py_cui](https://github.com/jwlodek/py_cui) library.
---
+++ py_cui-2048/2048.py
@@ -0,0 +1,73 @@
+class Game:
+
+    def __init__(self, initial_placement):
+        self.game_board = Board(initial_placement)
+        self.score = 0
+        self.turn = 0
+
+
+    def check_victory(self):
+        for row in self.game_board.board_positions:
+            for elem in row:
+                if elem == 2048:
+                    return True
+
+class Board:
+
+    def __init__(self, initial_placement):
+
+        initial_x1 = initial_placement[0][0]
+        initial_y1 = initial_placement[0][1]
+        initial_val1 = initial_placement[0][2]
+        initial_x2 = initial_placement[1][0]
+        initial_y2 = initial_placement[1][1]
+        initial_val2 = initial_placement[1][2]
+        self.board_positions = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+        self.board_positions[initial_y1][initial_x1] = initial_val1
+        self.board_positions[initial_y2][initial_x2] = initial_val2
+
+    def process_columns(self, direction):
+        for i in range(0, len(self.board_positions[0])):
+            temp = []
+            for j in range(0, len(self.board_positions)):
+                temp.append(self.board_positions[j][i])
+            if direction == 'up':
+                temp = temp.reverse()
+            self.update_nums(temp)
+            if direction == 'up':
+                temp.reverse()
+            for j in range(0, len(self.board_positions)):
+                self.board_positions[j][i] = temp[j]
+
+
+    def process_rows(self, direction):
+        for i in range(0, len(self.board_positions)):
+            temp = self.board_positions[i]
+            if direction == 'left':
+                temp = temp.reverse()
+            self.update_nums(temp)
+            if direction == 'left':
+                temp = temp.reverse()
+            self.board_positions[i] = temp
+
+
+    def update_nums(self, nums):
+        counter = 0
+        while counter < 3:
+            if nums[counter] = nums[counter + 1]:
+                nums[counter + 1] = 2 * nums[counter]
+                nums[counter] = 0
+                counter = counter + 1
+            counter = counter + 1
+        counter = 3
+        while counter >= 0:
+            if nums[counter] == 0:
+                i = counter - 1
+                while i >=0:
+                    nums[i] = nums[i + 1]
+            counter = counter - 1
+
+
+    def print_board(self):
+        for i in range(0, len(self.board_positions)):
+            print(self.board_positions[i])
