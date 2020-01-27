524c3d9d60292188e0b9ff63d4f5d370b08043a1
jwlodek <jwlodek@cs.stonybrook.edu>
2020-01-27 14:38:47 -0500

    Update setup and gitignore

--- setup.py
+++ setup.py
@@ -6,12 +6,18 @@ import os
 with open('requirements.txt', 'r') as req_fp:
     required_packages = req_fp.readlines()
 
+# Use README for long description
+with open('README.md', 'r') as readme_fp:
+    long_description = readme_fp.read()
+
 setup(
     name="py_cui_2048",
     version="0.0.1",
     author="Jakub Wlodek",
     author_email="jwlodek.dev@gmail.com",
     description="A command line version of the classic 2048 game, built with py_cui.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     license="BSD 3-Clause",
     keywords="tui command-line cli cui curses 2048",
     url="https://github.com/jwlodek/py_cui_2048",
